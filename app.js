0050e1d6afd38e59823651234037c94d34488a72
XhitHub <com.trojan.exe@gmail.com>
2021-11-11 18:32:50 +0800

    router/match

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var matchRouter = require('./routes/match');
 
 var app = express();
 
@@ -19,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// init routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/match', matchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/match.js
+++ routes/match.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/availableMatches', function(req, res, next) {
+  // get availableMatches by controller
+  res.send({
+    data: {
+      availableMatches: []
+    }
+  });
 });
+
+router.get('/t1', (req, res, next) => {
+  res.send(401);
+});
+
 
 module.exports = router;
