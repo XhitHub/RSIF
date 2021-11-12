28d358ef6014baa863d39a3c85b1fee7c6897ec8
XhitHub <com.trojan.exe@gmail.com>
2021-11-12 18:32:54 +0800

    making auth

--- app.js
+++ app.js
@@ -3,12 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+// var session = require("express-session"),
+const bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 var matchRouter = require('./routes/match');
 
 var app = express();
+
+
+// auth setup
+// dummy test username pw
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username == 'u1' && password == 'p1') {
+      let user = {
+        username: 'u1',
+        name: 'u1 name',
+      }
+      return done(null, user)
+    } else {
+      return done(null, false, { message: 'Invalid user.' });
+    }
+    // User.findOne({ username: username }, function(err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) {
+    //     return done(null, false, { message: 'Incorrect username.' });
+    //   }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+    //   return done(null, user);
+    // });
+  }
+));
+// app.use(session({ secret: "ugthisduhrfgeiru" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +57,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // init routers
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
-app.use('/match', matchRouter);
+app.use('/match', passport.authorize('local', { failureRedirect: '/login.html' }), matchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,5 +79,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
